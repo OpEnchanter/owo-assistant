2bfd0b3f1f398eb4963e301f5d0d0653b6114418
Tristan Greenhagen <openchanter@gmail.com>
2026-02-03 19:49:59 -0500

    resolve file issues

--- server/modules/homeassistant.ts
+++ server/modules/homeassistant.ts
@@ -1,14 +1,9 @@
 import { ModuleBase,  type ModuleResult } from "./module.ts"
 
-<<<<<<< HEAD
-export class homeassistant extends Module {
-    public override onQuery(): ModuleResult {
-=======
 export class Module extends ModuleBase {
     constructor () {super()}
 
     public override onQuery(query: String): ModuleResult {
->>>>>>> refs/remotes/origin/main
         return {response: "Home assistant module activated.", endRequest: true} as ModuleResult;
     }
 }
